03a749948836ee45710f6f25ce6789ebe923293a
Mitra <mitra.mejia@gmail.com>
2013-06-07 18:29:08 -0300

    Todo el projecto

---
+++ application/views/welcome_message.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Proyecto</title>
+    <meta charset="utf-8">
+    <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> -->
+    <link rel="stylesheet" href="assets/stylesheets/styles.css">
+
+</head>
+<body>
+<div id="page" class="row-fluid span12">
+    <div id="content" class="row-fluid span7 offset1">
+        <div id="search-wrapper" class="row-fluid span10 offset1">
+            <div id="navbar">
+                <ul class="nav nav-tabs">
+                    <li>
+                        <a href="#noticias"><i class="icon-globe icon-white"></i>Noticias</a>
+                    </li>
+                    <li>
+                        <a href="#lugares"><i class=" icon-map-marker icon-white"></i>Lugares</a>
+                    </li>
+                    <li>
+                        <a href="#estadisticas"><i class="icon-tasks icon-white"></i>Estadisticas</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="search">
+                <input id="input-search" class="span11" type="text" placeholder="Buscar noticias y novedades"/>
+                <button id="btn-search" class="span1" type="submit"><i class="icon-search icon-white" ></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="/javascripts/jquery.js"></script>
+<script src="assets/javascripts/main.js"></script>
+<script src="assets/javascripts/bootstrap-tab.js"></script>
+
+</body>
+</html>
